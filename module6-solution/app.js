79a83fb3b2d06158b6f667cca2212c376ce30d60
Casey <csmith20@gulls.salisbury.edu>
2021-10-08 14:42:44 -0400

    completes bonus portion

--- module6-solution/app.js
+++ module6-solution/app.js
@@ -1,5 +1,5 @@
 (function(){
-  "use strict"
+  'use strict';
 
   // Create app.js in your project and declare an Angular module to match your ng-app declaration.
   angular.module("LunchCheck", []).controller("LunchCheckController", LunchCheckController);
@@ -10,29 +10,47 @@
   LunchCheckController.$inject = ['$scope'];
 
   function LunchCheckController($scope){
-    $scope.text;
-    $scope.message;
-    $scope.button = function(){
-      if ( $scope.text === undefined ||
-        $scope.text.length === 0 ) {
-        $scope.message =  "Enter data first.";
+    $scope.message = "";
+    $scope.button = function (){
+
+    // If the message is "Enjoy!" or "Too much!", make the font color green. 
+    // If the message is "Please enter data first", make the font color red.
+    // If the message is "Enjoy!" or "Too much!", make the border color around the 
+    // textbox green. If the message is "Please enter data first", make the border 
+    // color around the textbox red.
+    $scope.messageColor = "green";
+    var numberOfItems = getNumberOfItems($scope.menu);
+    if (numberOfItems == 0) {
+      $scope.messageColor = "red";
+      $scope.message = "Please enter data first";
+    } else if (numberOfItems <= 3) {
+      $scope.message = "Enjoy!";
     } else {
+      $scope.messageColor = "red";
+      $scope.message = "Too much!";
+    }
+  };
 
-      var input = $scope.text.split(",");
-      var striped = [];
-      for( var i=0, j= input.length; i < j; i++ ) {
-        if ( input[i].trim() !== "" ) {
-          striped.push(input[i]);
-        }
-      }
-
-      if ( striped.length <= 3 ) {
-        $scope.message =  "Enjoy Lunch!";
-      } else {
-        $scope.message = "This is too much!";
-      }
+  // Implement this case item 1, item2,,item3 or this case item 1, item2, ,item3 as not 
+  // counting an 'empty' item towards the count of how many items there are in the list. 
+  // Please make sure to put a comment somewhere next to the input textbox stating that 
+  // you do NOT consider an empty item, i.e., , , as an item towards to the count, so 
+  // whoever is grading your assignment doesn't erroneously mark that as an error.
+  function getNumberOfItems(menu){
+    if(!menu){ 
+      return 0; 
     }
 
-  };
+    var menuArray = menu.split(',');
+    var menuArrayLength = menuArray.length;
+
+    for (var i = 0; i < menuArray.length; i++){
+      var trimmedInput = menuArray[i].trim();
+      if(!trimmedInput) {
+        menuArrayLength--;
+      }
+    }
+    return menuArrayLength;
+  }
 }
 })();
